f85e0522bacb0cf557a21c3172a8ba68161c4fcd
rendi897 <84292422+rendi897@users.noreply.github.com>
2025-02-20 01:10:47 +0700

    Delete features/db.js

--- features/db.js
+++
@@ -1,9 +0,0 @@
-const { Pool } = require("pg");
-require("dotenv").config();
-
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }  // Supabase & Neon butuh SSL
-});
-
-module.exports = pool;
