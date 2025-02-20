84c5ce2535c7aa0865ad6034c3f82da0bb1e98ec
rendi897 <84292422+rendi897@users.noreply.github.com>
2025-02-20 21:53:50 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,6 +1,5 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("./serviceAccountKey.json"); // Ganti dengan file JSON Anda
-
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
     databaseURL: "https://luffybotv12.firebaseio.com", // Ganti dengan URL proyek Anda
