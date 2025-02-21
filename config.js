48cd02b6e65e1841087b08f20068f77135a7690d
rendi897 <84292422+rendi897@users.noreply.github.com>
2025-02-21 23:14:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,5 +2,5 @@ module.exports = {
   admins: [6027790623],  // Daftar ID admin
   owners: [6027790623],  // Daftar ID owner
   users: [],  // Daftar ID user
-  group_id: []  // Daftar Grup yg diakses bot
+  group_id: [-1002301561001, -1002150325548]  // Daftar Grup yg diakses bot
 };
